3f024de7c2c6987083e85609283740ddbf70f222
dswdejonge <dsw.dejonge@gmail.com>
2019-12-09 16:40:50 +0100

    Change pre-run to pre_run. Update species attribute requirements. Refactor check format functions. Allow checks for both species and stations.

--- data-raw/check_format.R
+++ data-raw/check_format.R
@@ -36,3 +36,115 @@ get_int_att <- function(my_attributes){
   return(int_att)
 }
 
+get_boolean_att <- function(my_attributes){
+  bool_att <- my_attributes %>%
+    filter(Datatype == "Boolean")
+  return(bool_att)
+}
+
+are_required_att_present <- function(file, file_name, required_attributes){
+  att_is_missing <- !required_attributes$Attribute %in% colnames(file)
+  if(TRUE %in% (att_is_missing)){
+    stop(paste0("The required attribute(s) ", paste(required_attributes$Attribute[att_is_missing], collapse = ", "),
+                " is/are missing in the file ", file_name))
+  }
+}
+
+are_StationIDs_unique <- function(file, file_name){
+  ID_is_duplicated <- duplicated(file$StationID)
+  if(!"Species_reported" %in% colnames(file)){
+    if(TRUE %in% ID_is_duplicated){
+      stop(paste0("The StationID(s) ", paste(file$StationID[ID_is_duplicated], collapse = ", "),
+                  " in file ",file_name," is/are duplicated. StationID must be unique."))
+    }
+  }
+}
+
+are_alternative_required_att_present <- function(file, file_name, alternative_attributes){
+  # Is at least one of the alternatives for required fields present?
+  if(length(alternative_attributes) > 0){
+    present <- FALSE
+    for(j in 1:length(alternative_attributes)){
+      att_is_missing <- !alternative_attributes[[j]]$Attribute %in% colnames(file)
+      if(all(!att_is_missing)){
+        present <- TRUE
+      }
+    }
+    if(!present) {
+      stop(paste0("In ", file_name," please provide ",paste(alternative_attributes[[1]]$Attribute, collapse = ", ")," or an alternative set of required variables."))
+    }
+  }
+}
+
+are_att_names_valid <- function(file, file_name, my_attributes){
+  # Are all provided attributes names (incl. optional) valid?
+  att_is_invalid <- !colnames(file) %in% my_attributes$Attribute
+  if(TRUE %in% att_is_invalid){
+    stop(paste0("The attribute name(s) ",
+                paste(colnames(file)[att_is_invalid], collapse = ", ") ,
+                " in file ",file_name, " is/are not valid."))
+  }
+}
+
+are_req_att_complete <- function(file, file_name, required_attributes){
+  # No NA values are allowed in the required columns.
+  columns <- colnames(file) %in% required_attributes$Attribute
+  if(TRUE %in% is.na(file[,columns])){
+    rowi <- which(is.na(file[,columns]), arr.ind = T)[,"row"]+1
+    stop(paste0("NA values are not allowed for the required attributes ",
+                paste(required_attributes$Attribute, collapse = ", "),
+                ". Please check the file ", file_name,
+                " row(s): ",paste(rowi, collapse = ", ")))
+  }
+}
+
+are_alternative_req_att_complete <- function(file, file_name, alternative_attributes){
+  if(length(alternative_attributes) > 0){
+    # No NA values are allowed in at least one of the required alternatives
+    NA_index <- list()
+    for(j in 1:length(alternative_attributes)){
+      columns <- colnames(file) %in% alternative_attributes[[j]]$Attribute
+      NA_index[[j]] <- unique(which(is.na(file[,columns]), arr.ind = TRUE)[,"row"])
+    }
+    if(TRUE %in% duplicated(unlist(NA_index))){
+      stop(paste0("Every entry must have at least one complete set of alternative required fields. ",
+                  "Duplicate NAs are found in row(s) ",
+                  paste(unlist(NA_index)[duplicated(unlist(NA_index))]+1, collapse = ", "),
+                  " in file: ", file_name))
+    }
+  }
+}
+
+are_dates_converted <- function(file, file_name){
+  # All dates should be convertible from a string dd/mm/yyyy to R data format.
+  is_unconverted_date <- is.na(file$Date)
+  if(TRUE %in% is_unconverted_date){
+    stop(paste0("The date is row(s) ",paste(which(is_unconverted_date)+1, collapse = ", "),
+                " in file ", file_name,
+                " cannot be converted. Ensure all dates have the right format: dd/mm/yyyy."))
+  }
+}
+
+are_values_correct_type <- function(file, file_name, type_attributes, func){
+  columns <- which(colnames(file) %in% type_attributes$Attribute)
+  for(j in 1:length(columns)){
+    if(!func(file[,columns[j]])){
+      stop(paste0("The column ", colnames(file)[columns[j]], " in file ",file_name,
+                  " must contain only values of datatype ",type_attributes$Datatype[1]))
+    }
+  }
+}
+
+are_booleans_correct <- function(file, file_name, boolean_attributes){
+  if(length(boolean_attributes$Attribute) > 0){
+    columns <- which(colnames(file) %in% boolean_attributes$Attribute)
+    subset <- file[,columns]
+    is_not_boolean <- which(!is.na(subset) & subset != 0 & subset != 1, arr.ind = T)[,"row"]+1
+    if(length(is_not_boolean) > 0){
+      stop(paste0("In file ",file_name,
+                  " the boolean attributes ", paste(colnames(file)[columns], collapse = ", "),
+                  " may only be 0, 1, or NA. Please check row(s) ",
+                  paste(is_not_boolean, collapse = ", ")))
+    }
+  }
+}
--- data-raw/construct_database.R
+++ data-raw/construct_database.R
@@ -5,107 +5,42 @@
 library(dplyr)
 source("check_format.R")
 tables <- list(
-  Stations <- c("attributes_stations.csv"),
-  Species <- c("attributes_species.csv")
+  Stations <- list(folder = "Stations", att = "attributes_stations.csv"),
+  Species <- list(folder = "Species", att = "attributes_species.csv")
 )
 
 for(table in tables){
   # Read in attribute requirements
-  my_attributes <- read.csv(system.file("extdata", "attributes_stations.csv", package = "TripleD"))
+  my_attributes <- read.csv(system.file("extdata", table$att, package = "TripleD"))
 
   # Read in data
-  data <- import_data("Stations")
+  data <- import_data(table$folder)
 
   # Subset data requirements
   required_attributes <- get_required_att(my_attributes)
   alternative_attributes <- get_alternative_att(my_attributes)
   doubles_attributes <- get_doubles_att(my_attributes)
   integer_attributes <- get_int_att(my_attributes)
+  boolean_attributes <- get_boolean_att(my_attributes)
 
-  # Check requirements for each file
+  # Check format requirements for each file
   for(i in 1:length(data)){
     file <- data[[i]]
-    file_name <- names(data)[i]
+    file_name <- paste(table$folder, names(data)[i], sep = "/")
 
-    # Are all required attributes present?
-    att_is_missing <- !required_attributes$Attribute %in% colnames(file)
-    if(TRUE %in% (att_is_missing)){
-      stop(paste0("The required attribute(s) ", paste(required_attributes$Attribute[att_is_missing], collapse = ", "),
-                  "is/are missing in the file ", file_name))
+    are_required_att_present(file, file_name, required_attributes)
+    are_StationIDs_unique(file, file_name)
+    are_alternative_required_att_present(file, file_name, alternative_attributes)
+    are_att_names_valid(file, file_name, my_attributes)
+    are_req_att_complete(file, file_name, required_attributes)
+    are_alternative_req_att_complete(file, file_name, alternative_attributes)
+    if(table$folder == "Stations"){
+      data[[i]]$Date <- as.Date(data[[i]]$Date, format = "%d/%m/%Y")
+      are_dates_converted(file = data[[i]], file_name)
     }
-
-    # Are the stationIDs unique?
-    ID_is_duplicated <- duplicated(file$StationID)
-    if(TRUE %in% ID_is_duplicated){
-      stop(paste0("The StationID(s) ", paste(file$StationID[ID_is_duplicated], collapse = ", "),
-                  " is/are duplicated; StationID must be unique."))
-    }
-
-    # Is at least one of the alternatives for required fields present?
-    present <- FALSE
-    for(j in 1:length(alternative_attributes)){
-      att_is_missing <- !alternative_attributes[[j]]$Attribute %in% colnames(file)
-      if(all(!att_is_missing)){
-        present <- TRUE
-      }
-    }
-    if(!present) {
-      stop(paste0("Please provide at least one alternative set of required variables in ", file_name))
-    }
-
-    # Are all provided attributes names (incl. optional) valid?
-    att_is_invalid <- !colnames(file) %in% my_attributes$Attribute
-    if(TRUE %in% att_is_invalid){
-      stop(paste0("The attribute name(s) ",
-                  paste(colnames(file)[att_is_invalid], collapse = ", ") ,
-                  " in file ",file_name, " is not valid."))
-    }
-
-    # No NA values are allowed in the required columns.
-    columns <- colnames(file) %in% required_attributes$Attribute
-    if(TRUE %in% is.na(file[,columns])){
-      stop(paste0("NA values are not allowed for the required attributes ",
-                  paste(required_attributes$Attribute, collapse = ", "),
-                  ". Please check the file ", file_name))
-    }
-
-    # No NA values are allowed in at least one of the required alternatives
-    NA_index <- list()
-    for(j in 1:length(alternative_attributes)){
-      columns <- colnames(file) %in% alternative_attributes[[j]]$Attribute
-      NA_index[[j]] <- unique(which(is.na(file[,columns]), arr.ind = TRUE)[,"row"])
-    }
-    if(TRUE %in% duplicated(unlist(NA_index))){
-      stop(paste0("Every entry must have at least one complete set of alternative required fields. ",
-                  "Duplicate NAs are found in row(s) ",
-                  paste(unlist(NA_index)[duplicated(unlist(NA_index))], collapse = ", "),
-                  " in file: ", file_name))
-    }
-
-    # All dates should be convertible from a string dd/mm/yyyy to R data format.
-    data[[i]]$Date <- as.Date(data[[i]]$Date, format = "%d/%m/%Y")
-    is_unconverted_date <- is.na(data[[i]]$Date)
-    if(TRUE %in% is_unconverted_date){
-      stop(paste0("The date is row(s) ",paste(which(is_unconverted_date), collapse = ", "),
-                  " in file ", file_name,
-                  " cannot be converted. Ensure all dates have the right format: dd/mm/yyyy."))
-    }
-
-    # All attributes assigned 'Double' must be datatype double
-    columns <- which(colnames(file) %in% doubles_attributes$Attribute)
-    for(j in 1:length(columns)){
-      if(!is.double(file[,columns[j]])){
-        stop(paste0("The column ", colnames(file)[columns[j]], " must contain only values of datatype double i.e. no strings or integers."))
-      }
-    }
-
-    # All attributes assigned 'Integer' must be datatype double
-    columns <- which(colnames(file) %in% integer_attributes$Attribute)
-    for(j in 1:length(columns)){
-      if(!is.integer(file[,columns[j]])){
-        stop(paste0("The column ", colnames(file)[columns[j]], " must contain only values of datatype integer i.e. no strings or doubles."))
-      }
-    }
+    are_values_correct_type(file, file_name, doubles_attributes, func = is.double)
+    are_values_correct_type(file, file_name, integer_attributes, func = is.integer)
+    are_booleans_correct(file, file_name, boolean_attributes)
   }
 }
 
