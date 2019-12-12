e05ade117d657c06538ef8832e668397bb16b4ca
dswdejonge <dsw.dejonge@gmail.com>
2019-12-12 10:47:24 +0100

    Changed attributes file for extraction fractions and predefined units. Add code for complete groups, fractions, predefined units, and species metadata.

--- data-raw/construct_database.R
+++ data-raw/construct_database.R
@@ -3,6 +3,7 @@
 # -------------------------------------------------------
 
 library(dplyr)
+library(TripleD)
 source("check_format.R")
 tables <- list(
   Stations <- list(folder = "Stations", att = "attributes_stations.csv"),
@@ -22,25 +23,36 @@ for(table in tables){
   doubles_attributes <- get_doubles_att(my_attributes)
   integer_attributes <- get_int_att(my_attributes)
   boolean_attributes <- get_boolean_att(my_attributes)
+  fraction_attributes <- get_fraction_att(my_attributes)
+  predefined_attributes <- get_predefined_att(my_attributes)
 
   # Check format requirements for each file
   for(i in 1:length(data)){
     file <- data[[i]]
     file_name <- paste(table$folder, names(data)[i], sep = "/")
 
+    # Attribute presence
     are_required_att_present(file, file_name, required_attributes)
-    are_StationIDs_unique(file, file_name)
     are_alternative_required_att_present(file, file_name, alternative_attributes)
     are_att_names_valid(file, file_name, my_attributes)
-    are_req_att_complete(file, file_name, required_attributes)
-    are_alternative_req_att_complete(file, file_name, alternative_attributes)
+
+    # Correct values
+    are_StationIDs_unique(file, file_name)
+    are_required_att_complete(file, file_name, required_attributes)
+    are_alternative_required_att_complete(file, file_name, alternative_attributes)
     if(table$folder == "Stations"){
       data[[i]]$Date <- as.Date(data[[i]]$Date, format = "%d/%m/%Y")
       are_dates_converted(file = data[[i]], file_name)
     }
+    are_groups_complete(file, file_name, my_attributes)
     are_values_correct_type(file, file_name, doubles_attributes, func = is.double)
     are_values_correct_type(file, file_name, integer_attributes, func = is.integer)
     are_booleans_correct(file, file_name, boolean_attributes)
+    are_fractions_correct(file, file_name, fraction_attributes)
+    are_predefined_atts_correct(file, file_name, predefined_attributes)
+    if(table$folder == "Species"){
+      are_species_metadata_consistent(file, file_name)
+    }
   }
 }
 
@@ -49,13 +61,8 @@ for(table in tables){
 # - If there are any suspicious values (very deep water depths, very large organisms, strange locations).
 # - Track distances equal?
 # - are there any suspicious values (very small/large/heavy organisms)
-
+# If the species name is present in WoRMS: the newest accepted synonym is added in a column.
+# If the species name is not present in WoRMS (for example higher taxon): what then?
 # Add findings to construct database log file
 # - what data is missing
 # - weird values
-
-
-# Fraction, isFractionAssumed must be same for all species entries at a certain stationID.
-# If the species name is present in WoRMS: the newest accepted synonym is added in a column.
-# If the species name is not present in WoRMS (for example higher taxon): what then?
-# All size measurements have units.
