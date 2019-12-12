221aef3355ef4fb1a5f13ea3b45ebec3efe69c6d
dswdejonge <dsw.dejonge@gmail.com>
2019-12-12 14:10:56 +0100

    Remove redundant files.

--- data-raw/Stations.R
+++
@@ -1,9 +0,0 @@
-# Construct and update table with station data
-# --------------------------------------------
-
-
-areRequiredStationAttributesPresent <- function(input, req, alt) {
-  all(req %in% input)
-}
-#stations <- dplyr::bind_rows(data, .id = "File")
-#usethis::use_data(sampling_stations, overwrite = T)
