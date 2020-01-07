9473054c190357b4591106e9b9b54b5107638a47
dswdejonge <dsw.dejonge@gmail.com>
2020-01-07 11:12:59 +0100

    Add power function to get weight from length.

--- R/size_to_weight.R
+++ R/size_to_weight.R
@@ -1,5 +1,13 @@
-# entry is length in mm
-# value is weight Ash Free Dry Weight in g
+#' Calculate weight from length
+#'
+#' This function calculates Ash Free Dry Weight (g) from length (mm) using the power function
+#' weight = A * length^B.
+#' with the coefficients taken from regression lines.
+#' @param length Length of organism in mm.
+#' @param A The A factor in the power function A * length^B.
+#' @param B The B exponent in the power function A * length^B.
+#' @return Returns the weight as Ash Free Dry Weight (AFDW) in grams (g).
+#' @export
 length_to_weight <- function(length, A, B){
   weight = A * length^B
   return(weight)
