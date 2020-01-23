4237d0c07dc07a0eb6a52fc3e2727a1d1c524e9f
dswdejonge <dsw.dejonge@gmail.com>
2020-01-23 11:44:09 +0100

    App shows map and histograms.

--- TripleD/app.R
+++ TripleD/app.R
@@ -6,69 +6,108 @@
 #
 #    http://shiny.rstudio.com/
 #
+
 # install.packages("shiny)
 library(shiny)
 library(TripleD)
+library(ggplot2)
+library(mapdata)
+library(dplyr)
 
-# Define UI for application that plots abundance, and summarizes some data.
+# -----------------------
+# Define fixed parameters
+# -----------------------
+# North Sea boundaries lat and lon
+lats <- c(50, 62)
+lons <- c(-5, 10)
+
+
+# ---------------
+# User interface
+# ---------------
 ui <- navbarPage( # page with tabs to navigate to different pages
   "NIOZ TripleD Data",
-  # Page with interactive North Sea map
-  tabPanel("Map"),
 
+  # ------------------------------------
+  # Page with interactive North Sea map
+  # ------------------------------------
+  tabPanel(
+    "Map",
+    plotOutput("North_Sea_map")
+  ),
+
+  # --------------------
   # Page with histograms
+  # --------------------
   tabPanel(
     "Histograms",
     sidebarLayout(
       sidebarPanel(
-        selectInput("histogram_data", h3("Histogram of:"),
-                    choices = list("Water depth" = "Water_depth_m", "Track length" = "Track_dist_m",
-                                          "Taxon count" = "Count"), selected = 1)
+        selectInput(
+          "histogram_data",
+          label = h3("Histogram of:"),
+          choices = list(
+            "Water depth" = "Water_depth_m",
+            "Track length" = "Track_length_m",
+            "Taxon count" = "Count"),
+          selected = 1)
       ),
       mainPanel(
         plotOutput("histoPlot")
       )
     )
   ),
-  tabPanel(
-    "Some plots",
-    # Application title
-    titlePanel("NIOZ TripleD Data"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-      sidebarPanel(
-        sliderInput("bins","Number of bins:", min = 5, max = 50, value = 30)
-      ),
-
-    # Show a plot of the generated distribution
-      mainPanel(
-        plotOutput("distPlot")
-      )
-    )
-  ),
+  # -----------
+  # About page
+  # -----------
   tabPanel("About",
            p("This interactive page is created by Danielle de Jonge."),
            img(src = "Diagram_TripleD.png"))
 )
 
-# Define server logic required to draw a histogram
+# ---------------------------------------
+# Server, i..e R-code that renders output
+# ---------------------------------------
 server <- function(input, output) {
 
-  # Histogram plots
-  output$histoPlot <- renderPlot({
-    data_to_plot <- database[,input$histogram_data]
-    hist(data_to_plot)
+  # --------------
+  # North Sea map
+  # --------------
+  output$North_Sea_map <- renderPlot({
+    # Get coastal outlines
+    reg <- ggplot2::map_data("world2Hires")
+    reg <- reg %>%
+      dplyr::filter(
+        long > lons[1] &
+        long < lons[2] &
+        lat > lats[1] &
+        lat < lats[2]
+      )
+    # make plot
+    map_samples <- ggplot() +
+      # add coastline
+      geom_polygon(data = reg, aes(x = long, y = lat, group = group),
+                   fill = "darkgrey", color = NA) +
+      # add points
+      geom_point(data = stations_final, aes(x = Lon_DD, y = Lat_DD),
+                 fill = "red", colour = "black",
+                 size = 1, shape = 21) +
+      # configure projection and plot domain
+      coord_map(xlim = lons, ylim = lats) +
+      # formatting
+      ylab("") + xlab("")+
+      theme_bw()
+    map_samples
   })
 
-  output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- database$Water_depth_m
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'orange')
-   })
+  # --------------
+  # Histogram plots
+  # --------------
+  output$histoPlot <- renderPlot({
+    data_to_plot <- pull(database, input$histogram_data)
+    hist(data_to_plot)
+  })
 }
 
 # Run the application
