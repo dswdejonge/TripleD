c36f9c38942f22a1ae72fd96d12d1f6fd9e2205f
dswdejonge <dsw.dejonge@gmail.com>
2020-01-23 10:42:00 +0100

    App with tabs for pages, add histogram page, and image.

--- TripleD/app.R
+++ TripleD/app.R
@@ -10,35 +10,60 @@
 library(shiny)
 library(TripleD)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Define UI for application that plots abundance, and summarizes some data.
+ui <- navbarPage( # page with tabs to navigate to different pages
+  "NIOZ TripleD Data",
+  # Page with interactive North Sea map
+  tabPanel("Map"),
 
-   # Application title
-   titlePanel("NIOZ TripleD Data"),
-
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
+  # Page with histograms
+  tabPanel(
+    "Histograms",
+    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 5,
-                     max = 50,
-                     value = 30)
+        selectInput("histogram_data", h3("Histogram of:"),
+                    choices = list("Water depth" = "Water_depth_m", "Track length" = "Track_dist_m",
+                                          "Taxon count" = "Count"), selected = 1)
+      ),
+      mainPanel(
+        plotOutput("histoPlot")
+      )
+    )
+  ),
+  tabPanel(
+    "Some plots",
+    # Application title
+    titlePanel("NIOZ TripleD Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("bins","Number of bins:", min = 5, max = 50, value = 30)
       ),
 
-      # Show a plot of the generated distribution
+    # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        plotOutput("distPlot")
       )
-   )
+    )
+  ),
+  tabPanel("About",
+           p("This interactive page is created by Danielle de Jonge."),
+           img(src = "Diagram_TripleD.png"))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-   output$distPlot <- renderPlot({
+  # Histogram plots
+  output$histoPlot <- renderPlot({
+    data_to_plot <- database[,input$histogram_data]
+    hist(data_to_plot)
+  })
+
+  output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
+      x    <- database$Water_depth_m
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
       # draw the histogram with the specified number of bins
