037ae8eb2da7cdcdc21c80f3970d94c618697c30
dswdejonge <dsw.dejonge@gmail.com>
2020-06-05 10:24:56 +0200

    Add documentation to community matrix function.

--- R/create_community_matrix.R
+++ R/create_community_matrix.R
@@ -1,13 +1,41 @@
-
-get_community_matrix <- function(
-  database, taxonomic_level, abundance_measure, include_incomplete = TRUE,
-  taxonomic_level_options = c("all","species","genus","family","order","class","phylum"),
-  abundance_measure_options = c("presence_absence","Density_nr_per_m2","Biomass_g_per_m2","Density_nr_per_m3","Biomass_g_per_m3")
-  ){
+#' Get community matrix from the TripleD database
+#'
+#' This functions lets you extract a community matrix
+#' (i.e. stations in rows, taxa in columns) from the TripleD databas.
+#' @param database A dataframe, database output from the TripleD package.
+#' @param taxonomic_level The taxonomic level for which you want to create
+#' a community matrix. Options: \itemize{
+#' \item{all}
+#' \item{species (default)}
+#' \item{genus}
+#' \item{family}
+#' \item{order}
+#' \item{class}
+#' \item{phylum}
+#' }
+#' @param abundance_measure The value in the cells. Options: \describe{
+#' \item{presence_absence}{only 1 (present) or 0 (absence)}
+#' \item{Density_nr_per_m2}{individual count per square meter}
+#' \item{Biomass_g_per_m2"}{ash-free dry weight in grams per square meter}
+#' \item{Density_nr_per_m3}{individual count per cubic meter}
+#' \item{Biomass_g_per_m3}{ash-free dry weight in grams per cubic meter}
+#' }
+#' @param include_incomplete Include values based on incomplete data
+#' i.e. underestimations? (Default TRUE)
+#' @return Returns a dataframe with StationID as first row.
+#' @details Missing data filled with 0.
+#' @examples
+#' get_community_matrix(database, "species", "Biomass_g_per_m2")
+#' get_community_matrix(database, "phylum", "Density_nr_per_m2", include_incomplete = FALSE)
+#' @export
+get_community_matrix <- function(database, taxonomic_level, abundance_measure,
+                                 include_incomplete = TRUE){
   # Check input
-  if(!taxonomic_level %in% taxonomic_level_options){
+  if(!taxonomic_level %in%
+     c("all","species","genus","family","order","class","phylum")){
     stop("Provide a valid taxonomic level.")
-  }else if(!abundance_measure %in% abundance_measure_options){
+  }else if(!abundance_measure %in%
+           c("presence_absence","Density_nr_per_m2","Biomass_g_per_m2","Density_nr_per_m3","Biomass_g_per_m3")){
     stop("Provide a valid abundance measure.")
   }
 
@@ -35,17 +63,16 @@ get_community_matrix <- function(
   if(!include_incomplete){
     if(abundance_measure == "Density_nr_per_m2" | abundance_measure == "Density_nr_per_m3"){
       mysubset <- database %>%
-        dplyr::filter(incomplete_count == TRUE)
+        dplyr::filter(incomplete_count == !include_incomplete)
     }else if(abundance_measure == "Biomass_g_per_m2" | abundance_measure == "Biomass_g_per_m3"){
       mysubset <- database %>%
-        dplyr::filter(incomplete_biomass == TRUE)
+        dplyr::filter(incomplete_biomass == !include_incomplete)
     }
   }else{
     mysubset <- database
   }
 
   mysubset <- mysubset %>%
-    dplyr::select(StationID, !!dplyr::sym(taxon_column), !!dplyr::sym(abundance_measure)) %>%
     dplyr::group_by(StationID, !!dplyr::sym(taxon_column)) %>%
     dplyr::summarise(Value = sum(!!dplyr::sym(abundance_measure)))
 
